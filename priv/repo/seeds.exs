9bc58799e112d9224c334346ae50c19a77655341
Rhett Del Fierro <thinquetank@gmail.com>
2016-10-07 21:36:08 -0700

    added seed data and ran mix run priv/repo/seeds.exs to store it.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,32 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+
+#DUMMMMMMY DATA. If you want more we can add more to the list.:
+alias Hypeapp.{Repo, User}
+
+# Note the pipe operators. This entry will get piped as the first argument
+# into Enum.map/2. The shorthand notation means some the element (a changeset)
+# is the iteration over in the map wil be the 2nd argument to the changeset.
+# once that Enum.map is done, the whoel thing will go to Enum.each
+# where again one element (individual changeset) at a
+# time goes into repo.insert. See below:
+[
+  %{
+    first_name: "Some",
+    last_name: "User",
+    email: "someuser@hypeapp.com",
+    password: "someuserpassword"
+  },
+  %{
+    first_name: "Some",
+    last_name: "User2",
+    email: "someuser2@hypeapp.com",
+    password: "someuserpassword2"
+  },
+]
+# Shorthand for: Enum.map(structs, fn struct -> User.changeset(%User{}, struct) end
+|> Enum.map(&User.changeset(%User{}, &1))
+# Shorthand for: Enum.each(structs, fn struct -> Repo.insert(%User{}, struct) end
+|> Enum.each(&Repo.insert!(&1))
