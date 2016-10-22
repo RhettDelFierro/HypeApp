d7e65e02dc058e4f08620e271475a9bcbe41e19f
Rhett Del Fierro <thinquetank@gmail.com>
2016-10-21 20:36:43 -0700

    refactored votes to get composable queries.

--- web/models/vote.ex
+++ web/models/vote.ex
@@ -12,29 +12,54 @@ defmodule Hypeapp.Vote do
 
   @doc """
   Builds a changeset based on the `struct` and `params`.
+
   """
   def changeset(struct, params \\ %{}) do
     struct
+      #I do not think cast_assoc is right here. It needs params too.
       |> cast_assoc(:user)
       |> cast_assoc(:vote_type)
       |> cast_assoc(:places)
   end
 
+  def get_votes(place_id) do
+     __MODULE__ |> where([v], v.place_id == ^place_id)
+  end
 
-    def recent_feed(place_id) do
-       from v in __MODULE__,
-        where: v.place_id == ^place_id > ago(-1, "hour")
+  def get_recent_votes(query) do
+    query
+      |> where([v], v.place_id > ago(-1, "hour"))
+  end
 
-        order[by: [asc: :inserted_at]]
+  def join_votes_and_reviews(place_id) do
+     query |> join(:inner, [v], r in Review, v.place_id === r.place_id)
+  end
 
-    end
+  def get_votes_and_reviews(query) do
+    query |> order_by([v,r], asc: v.inserted_at) 
+  end
 
-    def check_trending(place_id) do
-       from v in __MODULE__,
-        where: v.place_id == ^place_id >
-         ago(-1, "hour")
-        select: count(:id)
-    end
+  def get_recent_feed(query) do
+    quer |> select([v,r], {v.user_id, })
+  end
+
+
+  def recent_feed(place_id) do
+     from v in __MODULE__,
+      join: r in Review, on: v.place_id == ^place_id > ago(-1, "hour")
+      order_by: [dsc: :inserted_at]],
+
+      from c in Comment,
+  join: p in Post, on: c.post_id == p.id,
+  select: {p.title, c.text}
+  end
+
+  def check_trending(place_id) do
+     from v in __MODULE__,
+      where: v.place_id == ^place_id >
+       ago(-1, "hour")
+      select: count(:id)
+  end
 
     #TEST THIS OUT.
     # def check_tranding(place_id) do
